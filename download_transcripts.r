9b3cec63baf53b390c012dcc73e3ad2185a38b5f
Nicholas Erskine <nicholas.erskine95@gmail.com>
2018-04-24 15:18:55 +1000

    only download fragments that haven't already been stored

--- download_transcripts.r
+++ download_transcripts.r
@@ -1,16 +1,19 @@
 #Script to download transcripts from the NSW Hansard.
 #Uses API documented at http://parliament-api-docs.readthedocs.io/en/latest/new-south-wales/#get-hansard-by-year
 
+#ToDo : only try to download pages that haven't already been downloaded
+
 library(purrr)
 library(readr)
+library(stringr)
 suppressMessages(library(jsonlite))
 suppressMessages(library(lubridate))
 
 
 
-begin <- ymd('2010-02-01')
+begin <- ymd('2010-02-20')
 
-end <- ymd('2010-05-01')
+end <- ymd('2010-02-25')
 
 days <- seq(begin,end,1) %>%
 	as.character()
@@ -33,24 +36,25 @@ document_descriptions <- days %>%
 document_ids <- map_chr(document_descriptions,'DocumentId')
 set_names(document_ids,map_chr(document_descriptions,'Text'))
 
-document_descriptions
+already_seen <- dir('./data') %>%
+	str_split(pattern='\\.') %>%
+	map_chr(~.x[[1]])
 
-#document_ids <- map(days,~fromJSON(.x,simplifyDataFrame=FALSE)) %>%
-#	rapply(f=I) %>% .[grep(.,pattern='HANSARD')]
-
-#document_ids <- document_ids[names(document_ids) != 'data.PdfDocumentId']
+document_ids <- document_ids[!(document_ids %in% already_seen)]
 
 transcript_url <- function(docID){
     paste0('https://api.parliament.nsw.gov.au/api/hansard/search/daily/fragment/',docID)
 }
 
 docs <- map(document_ids,~try(transcript_url(.x) %>%
-    read_file())
+    url %>%
+    read_file()
+    ) 
 )
 
 docs <- set_names(docs,document_ids)
 
 docs <- keep(docs,~class(.x)!='try-error')
 
-map2(docs,names(docs),~write_file(.x,path=paste0(.y,'.xml')))
+map2(docs,names(docs),~write_file(.x,path=paste0('./data/',.y,'.xml')))
 
