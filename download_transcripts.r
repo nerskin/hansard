176caff6699034248bb7113eae09c50d9251720e
Nicholas Erskine <nicholas.erskine95@gmail.com>
2018-04-24 16:07:28 +1000

    write transcripts as they are downloaded to avoid lost progress if script is interrupted

--- download_transcripts.r
+++ download_transcripts.r
@@ -17,27 +17,27 @@ begin <- ymd('2010-01-01')
 end <- ymd('2010-06-01')
 
 days <- seq(begin,end,1) %>%
-	as.character()
+  as.character()
 
 pb <- progress_bar$new(
-		       format = "getting document ids [:bar] :percent in :elapsed",
-		       total = length(days), clear=FALSE,width=60
-		       )
+  format = "getting document ids [:bar] :percent in :elapsed",
+  total = length(days), clear=FALSE,width=60
+)
 
 
 days <- map(days, function(x) {
-		    res <- paste0('https://api.parliament.nsw.gov.au/api/hansard/search/bydate?date=',x) %>%
-			    url %>%
-			    read_file()
-		    pb$tick()
-		    res
-		       }
+  res <- paste0('https://api.parliament.nsw.gov.au/api/hansard/search/bydate?date=',x) %>%
+    url %>%
+    read_file()
+  pb$tick()
+  res
+}
 )
 
 #days <- map(days,~paste0('https://api.parliament.nsw.gov.au/api/hansard/search/bydate?date=',.x) %>%
 #	     url %>%
 #	     read_file()
- #    ) %>%
+#    ) %>%
 #	keep(~.x != "{\"data\":[]}") 
 
 document_descriptions <- days %>%
@@ -53,39 +53,26 @@ document_ids <- map_chr(document_descriptions,'DocumentId')
 names(document_ids) <- map_chr(document_descriptions,'Text')
 
 already_seen <- dir('./data') %>%
-	str_split(pattern='\\.') %>%
-	map_chr(~.x[[1]])
+  str_split(pattern='\\.') %>%
+  map_chr(~.x[[1]])
 
 document_ids <- document_ids[!(document_ids %in% already_seen)]
 
 transcript_url <- function(docID){
-    paste0('https://api.parliament.nsw.gov.au/api/hansard/search/daily/fragment/',docID)
+  paste0('https://api.parliament.nsw.gov.au/api/hansard/search/daily/fragment/',docID)
 }
 
 pb <- progress_bar$new(
-		       format = "downloading documents [:bar] :percent in :elapsed",
-		       total = length(document_ids),clear=FALSE,width=60
-		       )
-
-docs <- map(document_ids,function(x){
-		    res <- try(transcript_url(x) %>%
-			url %>%
-			read_file()
-		)
-		    pb$tick()
-		    res
-}
+  format = "downloading documents [:bar] :percent in :elapsed",
+  total = length(document_ids),clear=FALSE,width=60
 )
 
-#docs <- map(document_ids,~try(transcript_url(.x) %>%
-#    url %>%
-#    read_file()
-#    ) 
-#)
-
-docs <- set_names(docs,document_ids)
-
-docs <- keep(docs,~class(.x)!='try-error')
-
-map2(docs,names(docs),~write_file(.x,path=paste0('./data/',.y,'.xml')))
-
+map(document_ids,function(x){
+  res <- try(transcript_url(x) %>%
+               url %>%
+               read_file()
+  )
+  write_file(res,path=paste0('./data/',.x,'.xml'))
+  pb$tick()
+}
+)
