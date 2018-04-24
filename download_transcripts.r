eb45d687f88b3b4b966856306366b04612f484a8
Nicholas Erskine <nicholas.erskine95@gmail.com>
2018-04-24 15:59:05 +1000

    Add cute progress bars

--- download_transcripts.r
+++ download_transcripts.r
@@ -1,28 +1,44 @@
 #Script to download transcripts from the NSW Hansard.
 #Uses API documented at http://parliament-api-docs.readthedocs.io/en/latest/new-south-wales/#get-hansard-by-year
 
-#ToDo : only try to download pages that haven't already been downloaded
+
 
 library(purrr)
 library(readr)
 library(stringr)
+library(progress)
 suppressMessages(library(jsonlite))
 suppressMessages(library(lubridate))
 
 
 
-begin <- ymd('2010-02-20')
+begin <- ymd('2010-01-01')
 
-end <- ymd('2010-02-25')
+end <- ymd('2010-06-01')
 
 days <- seq(begin,end,1) %>%
 	as.character()
 
-days <- map(days,~paste0('https://api.parliament.nsw.gov.au/api/hansard/search/bydate?date=',.x) %>%
-	     url %>%
-	     read_file()
-     ) %>%
-	keep(~.x != "{\"data\":[]}") 
+pb <- progress_bar$new(
+		       format = "getting document ids [:bar] :percent in :elapsed",
+		       total = length(days), clear=FALSE,width=60
+		       )
+
+
+days <- map(days, function(x) {
+		    res <- paste0('https://api.parliament.nsw.gov.au/api/hansard/search/bydate?date=',x) %>%
+			    url %>%
+			    read_file()
+		    pb$tick()
+		    res
+		       }
+)
+
+#days <- map(days,~paste0('https://api.parliament.nsw.gov.au/api/hansard/search/bydate?date=',.x) %>%
+#	     url %>%
+#	     read_file()
+ #    ) %>%
+#	keep(~.x != "{\"data\":[]}") 
 
 document_descriptions <- days %>%
   map(~fromJSON(.x,simplifyDataFrame = FALSE)) %>%
@@ -34,7 +50,7 @@ document_descriptions <- days %>%
 
 
 document_ids <- map_chr(document_descriptions,'DocumentId')
-set_names(document_ids,map_chr(document_descriptions,'Text'))
+names(document_ids) <- map_chr(document_descriptions,'Text')
 
 already_seen <- dir('./data') %>%
 	str_split(pattern='\\.') %>%
@@ -46,11 +62,26 @@ transcript_url <- function(docID){
     paste0('https://api.parliament.nsw.gov.au/api/hansard/search/daily/fragment/',docID)
 }
 
-docs <- map(document_ids,~try(transcript_url(.x) %>%
-    url %>%
-    read_file()
-    ) 
+pb <- progress_bar$new(
+		       format = "downloading documents [:bar] :percent in :elapsed",
+		       total = length(document_ids),clear=FALSE,width=60
+		       )
+
+docs <- map(document_ids,function(x){
+		    res <- try(transcript_url(x) %>%
+			url %>%
+			read_file()
+		)
+		    pb$tick()
+		    res
+}
 )
+
+#docs <- map(document_ids,~try(transcript_url(.x) %>%
+#    url %>%
+#    read_file()
+#    ) 
+#)
 
 docs <- set_names(docs,document_ids)
 
